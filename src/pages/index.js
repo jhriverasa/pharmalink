83f4725476078407bee5978b7d8642a3f97a648e
Jhonatan R. Saumeth <jhriverasa@unal.edu.co>
2021-05-20 17:05:52 -0500

    Juan's first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <div>
       <div className="font-bold">Hello world</div>
-      <div className="font-light">otro div</div>
+      <div className="font-light">ESTA ES LA RAMA DE JUAN</div>
     </div>
   );
 };
