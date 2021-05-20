5b1a9a816484b7dbabe0be14c650dffc04755c86
Jhonatan R. Saumeth <jhriverasa@unal.edu.co>
2021-05-20 17:07:33 -0500

    Mitchell's first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <div>
       <div className="font-bold">Hello world</div>
-      <div className="font-light">ESTA ES LA RAMA DE JUAN</div>
+      <div className="font-light">ESTA ES LA RAMA DE MITCHELL</div>
     </div>
   );
 };
