762e900082f2bca639aeeaf0451424e0b4c36c31
Jhonatan R. Saumeth <jhriverasa@unal.edu.co>
2021-05-20 17:02:31 -0500

    minor changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,19 @@
 import "tailwindcss/tailwind.css";
+import { Fragment } from "react";
+import Head from "next/head";
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Pharmalink</title>
+        <meta name="description" content="Created By PL" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </Fragment>
+  );
 };
 
 export default MyApp;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,6 @@
-import Head from "next/head";
-
 const Home = () => {
   return (
     <div>
-      <Head>
-        <title>Pharmalink</title>
-        <meta name="description" content="Created By PL" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <div className="font-bold">Hello world</div>
       <div className="font-light">otro div</div>
     </div>
